f2c513acb7585d12a7c1e214441254b10fe5a9c4
Bryan Yunis <bryany@booj.com>
2020-07-20 07:55:10 -0600

    add notFound middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Router = require('./routes')
 const morgan = require('morgan')
 const database = require('./db')
 const bodyParser = require('body-parser');
+const middlewares = require('./middlewares')
 
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(helmet())
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
+app.use(middlewares.notFound)
 app.use(Router)
 
 
